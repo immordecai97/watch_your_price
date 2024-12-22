c02c1e737a1b9e7185e54875fd7a6ebd58bf2655
Carlos <carlosscripto@gmail.com>
2024-12-21 21:54:59 -0500

    add Vercel configuration; update .gitignore to include Vercel; clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
     plugins: [react()],
